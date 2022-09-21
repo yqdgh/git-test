72af4454188c2f71e977ccca6b500f83ad0f59ab
yqdgh <88584948+yqdgh@users.noreply.github.com>
2022-09-21 15:14:21 +0800

    pull commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -14,5 +14,6 @@ public class GitTest {
         System.out.println("hot-test!");
         System.out.println("push test!");
         System.out.println("pull test!");
+        System.out.println("pull test!");
     }
 }
